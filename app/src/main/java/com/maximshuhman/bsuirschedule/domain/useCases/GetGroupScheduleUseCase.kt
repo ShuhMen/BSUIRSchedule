28822d551f122a6ace244edb4f725c9e18359b09
Maxim Shuhman <59605174+ShuhMen@users.noreply.github.com>
2025-12-26 13:44:01 +0300

    Отладка загрузки расписания и деталей занятия преподавателя (#5)

--- app/src/main/java/com/maximshuhman/bsuirschedule/domain/useCases/GetGroupScheduleUseCase.kt
+++ app/src/main/java/com/maximshuhman/bsuirschedule/domain/useCases/GetGroupScheduleUseCase.kt
@@ -46,17 +46,35 @@ class GetGroupScheduleUseCase @Inject constructor(
             val configureResult = configureSchedule((dbSchedule as AppResult.Success).data)
             val configureExams = configureExams(dbSchedule.data)
 
-            if(configureResult is AppResult.ApiError){
-                emit(configureResult)
-                return@flow
+            when{
+                configureResult is AppResult.ApiError && configureExams is AppResult.ApiError -> {
+                    emit(configureResult)
+                    return@flow
+                }
+                configureResult is AppResult.Success && configureExams is AppResult.ApiError ->{
+                    emit(AppResult.Success(GroupReadySchedule(group, configureResult.data, listOf())))
+                    return@flow
+                }
+                configureResult is AppResult.ApiError && configureExams is AppResult.Success ->{
+                    emit(AppResult.Success(GroupReadySchedule(group, listOf(), configureExams.data)))
+                    return@flow
+                }
+                else -> {
+                    emit(AppResult.Success(GroupReadySchedule(group, (configureResult as AppResult.Success).data, (configureExams as AppResult.Success).data)))
+                }
+
             }
 
-            if(configureExams is AppResult.ApiError){
+
+
+
+/*
+            if(configureResult is AppResult.ApiError && configureExams is AppResult.ApiError){
                 emit(configureExams)
                 return@flow
             }
+*/
 
-            emit(AppResult.Success(GroupReadySchedule(group, (configureResult as AppResult.Success).data, (configureExams as AppResult.Success).data)))
         }
 
         if(networkStatusTracker.getCurrentNetworkStatus() is NetworkStatus.Unavailable){
@@ -85,20 +103,24 @@ class GetGroupScheduleUseCase @Inject constructor(
         val configureResult = configureSchedule((dbSchedule as AppResult.Success).data)
         val configureExams = configureExams(dbSchedule.data)
 
-        if(configureResult is AppResult.ApiError){
-            emit(configureResult)
-            return@flow
+        when{
+            configureResult is AppResult.ApiError && configureExams is AppResult.ApiError -> {
+                emit(configureResult)
+                return@flow
+            }
+            configureResult is AppResult.Success && configureExams is AppResult.ApiError ->{
+                emit(AppResult.Success(GroupReadySchedule(group, configureResult.data, listOf())))
+                return@flow
+            }
+            configureResult is AppResult.ApiError && configureExams is AppResult.Success ->{
+                emit(AppResult.Success(GroupReadySchedule(group, listOf(), configureExams.data)))
+                return@flow
+            }
+            else -> {
+                emit(AppResult.Success(GroupReadySchedule(group, (configureResult as AppResult.Success).data, (configureExams as AppResult.Success).data)))
+            }
+
         }
-
-        if(configureExams is AppResult.ApiError){
-            emit(configureExams)
-            return@flow
-        }
-
-
-
-        emit(AppResult.Success(GroupReadySchedule(group, (configureResult as AppResult.Success).data, (configureExams as AppResult.Success).data)))
-
     }
 
     suspend inline fun analyseSchedule(group: Group, schedule: AppResult<CommonSchedule, SourceError>): AppResult<GroupReadySchedule, LogicError>  {
